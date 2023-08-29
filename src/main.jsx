a9a98f8396f112c6687b9fb1d6c86899d12294b0
Beto <ivjalberto@gmail.com>
2023-08-29 16:44:20 -0600

    Clase 264

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App.jsx'
 import './index.css'
+import { PokemonApp } from './PokemonApp.jsx'
 import { store } from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={ store }>
-      <App />
+      {/* <App /> */}
+      <PokemonApp/>
     </Provider>
   </React.StrictMode>,
 )
